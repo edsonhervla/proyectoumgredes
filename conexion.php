0009009d1705e5731a1b5e8541fadf271a86d6cc
edsonhervla <edsonlastor1996@gmail.com>
2023-06-07 23:09:29 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-$conexion=mysqli_connect('containers-us-west-173.railway.app','mysql://root:Sa6Jje9a0OvvF6SM7pUB@containers-us-west-173.railway.app:6730/railway','Sa6Jje9a0OvvF6SM7pUB','railway') or die ('Error al conectar con la base de datos');
+$conexion=mysqli_connect('localhost','id20881395_edsonhervla','321/Edson','id20881395_analisisuno') or die ('Error al conectar con la base de datos');
 ?>
