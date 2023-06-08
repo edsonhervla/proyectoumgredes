d678227450c590b0f4e4738c13c2130698ca3bdc
edsonhervla <edsonlastor1996@gmail.com>
2023-06-08 00:13:40 -0600

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,3 +1,8 @@
 <?php
-$db=new PDO("mysql:host=localhost; dbname=analisisuno", "root", "");    
+$db=new PDO("mysql:host=https://databases.000webhost.com/index.php?route=/; dbname=id20881395_analisisuno", "id20881395_edsonhervla", "321/Edson");    
 ?>
+
+
+<!-- ?php
+$db=new PDO("mysql:host=localhost; dbname=analisisuno", "root", "");    
+? -->
