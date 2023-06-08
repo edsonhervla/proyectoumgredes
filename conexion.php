64c00d61b12ee0e1a5475fa0cc5f4c94ede07436
edsonhervla <edsonlastor1996@gmail.com>
2023-06-07 23:57:12 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
-<!-- ?php
-$conexion=mysqli_connect('https://databases.000webhost.com/index.php?route=/database/structure&db=id20881395_analisisuno#','id20881395_edsonhervla@2a02:4780:bad:c0de::13','321/Edson','id20881395_analisisuno') or die ('Error al conectar con la base de datos');
-? -->
-
 <?php
+$conexion=mysqli_connect('localhost/https://databases.000webhost.com/index.php?route=/database/structure&db=id20881395_analisisuno#','id20881395_edsonhervla@2a02:4780:bad:c0de::13','321/Edson','id20881395_analisisuno') or die ('Error al conectar con la base de datos');
+?>
+
+<!-- ?php
 $servername = "localhost/https://databases.000webhost.com/index.php?route=/database/structure&db=id20881395_analisisuno#','id20881395_edsonhervla@2a02:4780:bad:c0de::13";
 $database = "id20881395_analisisuno";
 $username = "id20881395_edsonhervla";
@@ -15,4 +15,4 @@ if (!$conn) {
 }
 echo "Connected successfully";
 mysqli_close($conn);
-?>
+? -->
