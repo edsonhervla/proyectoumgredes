c43824960e0854ebb19797124d372f6d111cc2c8
edsonhervla <edsonlastor1996@gmail.com>
2023-06-07 23:20:37 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-$conexion=mysqli_connect('localhost','id20881395_edsonhervla','321/Edson','id20881395_analisisuno') or die ('Error al conectar con la base de datos');
+$conexion=mysqli_connect('https://databases.000webhost.com/index.php?route=/database/structure&db=id20881395_analisisuno#','id20881395_edsonhervla','321/Edson','id20881395_analisisuno') or die ('Error al conectar con la base de datos');
 ?>
