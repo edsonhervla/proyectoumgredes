f9258311a146ea14434f970146fbf7f85c34ca30
edsonhervla <edsonlastor1996@gmail.com>
2023-06-08 23:42:58 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,3 +1,4 @@
 <?php
-$conexion=mysqli_connect('predes.mysql.database.azure.com', 'predes', 'Aa123456', 'analisisuno') or die ('Error al conectar con la base de datos');
+$conexion=mysqli_init();
+mysqli_real_connect($conexion,"predes.mysql.database.azure.com","predes","Aa123456","analisisuno",3306) or die ('Error al conectar con la base de datos');
 ?>
