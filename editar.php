c19fd935ff9207dba075196ffb99b0ca7ef8b1f4
edsonhervla <edsonlastor1996@gmail.com>
2023-06-08 23:15:40 -0600

    Update editar.php

--- editar.php
+++ editar.php
@@ -5,6 +5,7 @@ $nomprod=$_POST['nomprod'];
 $precio=$_POST['precio'];
 
 //$db=new PDO("mysql:host=localhost; dbname=analisisuno;", "root","");
+//$db=new PDO("mysql:host=predes.mysql.database.azure.com; dbname=analisisuno;", "predes","Aa123456");
 
  if ($db->query("UPDATE productos SET nomprod='$nomprod', precio=$precio WHERE idprod='$idprod'")) 
  {
