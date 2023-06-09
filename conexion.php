51e865c3f7630393e15d8a390081d7c71629979a
edsonhervla <edsonlastor1996@gmail.com>
2023-06-08 22:53:16 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,3 @@
 <?php
-$conexion=mysqli_connect('https://databases.000webhost.com/index.php?route=/&route=%2F', 'id20881395_edsonhervla', '321/Edson', 'id20881395_analisisuno') or die ('Error al conectar con la base de datos');
+$conexion=mysqli_connect('predes.mysql.database.azure.com', 'predes', 'Aa123456', 'analisisuno') or die ('Error al conectar con la base de datos');
 ?>
-
-<!-- ?php
-$servername = "localhost/https://databases.000webhost.com/index.php?route=/database/structure&db=id20881395_analisisuno#','id20881395_edsonhervla@2a02:4780:bad:c0de::13";
-$database = "id20881395_analisisuno";
-$username = "id20881395_edsonhervla";
-$password = "321/Edson";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-mysqli_close($conn);
-? -->
